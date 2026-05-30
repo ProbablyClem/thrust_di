bee885275ae6258c77917c3fed7ad5fac29193f2
Clément Guiton <clement.guiton@itsf.io>
2026-05-30 23:02:58 +0200

    suppress dead code on service

--- thrust-macros/src/lib.rs
+++ thrust-macros/src/lib.rs
@@ -2,5 +2,8 @@ use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
 pub fn service(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+    let allow: TokenStream = "#[allow(dead_code)]".parse().unwrap();
+    let mut out = allow;
+    out.extend(item);
+    out
 }
